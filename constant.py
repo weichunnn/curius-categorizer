242a0199d369dad5b966ee8fd5fbe99a079ebe47
Wei Chun <tanweichun@outlook.com>
2024-04-16 23:31:22 +0800

    add constant

---
+++ constant.py
@@ -0,0 +1,7 @@
+API_BASE_URL = "https://curius.app/api"
+BATCH_SIZE = 50
+HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": "REPLACE_WITH_YOUR_OWN",
+}
+PARALLEL_WORKER_COUNT = 10
